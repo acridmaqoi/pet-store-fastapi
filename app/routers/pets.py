ae863e96c7d6c29092b7d1b433b9804e6a2f3e75
Sam Quinn <samquinn20@gmail.com>
2022-01-16 21:56:27 +0000

    rename pet_db to pet_utils

--- app/routers/pets.py
+++ app/routers/pets.py
@@ -1,10 +1,9 @@
 from datetime import date, datetime
 
 from fastapi import APIRouter, Response, HTTPException, Depends, status
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
-from ..internal import pet_db
+from ..internal import pet_utils
 
 from ..schemas import pets
 from ..internal.database import get_db
@@ -14,12 +13,12 @@ router = APIRouter()
 
 @router.post("")
 def create_pet(pet: pets.PetCreate, db: Session = Depends(get_db)):
-    return pet_db.create_pet(db=db, pet=pet)
+    return pet_utils.create_pet(db=db, pet=pet)
 
 
 @router.get("/{pet_id}", response_model=pets.Pet)
 def get_pet(pet_id: int, db: Session = Depends(get_db)):
-    db_pet = pet_db.get_pet(db=db, pet_id=pet_id)
+    db_pet = pet_utils.get_pet(db=db, pet_id=pet_id)
     if db_pet is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return db_pet
@@ -27,16 +26,16 @@ def get_pet(pet_id: int, db: Session = Depends(get_db)):
 
 @router.put("/{pet_id}")
 def update_pet(pet_id: int, pet: pets.PetCreate, db: Session = Depends(get_db)):
-    db_pet = pet_db.get_pet(db=db, pet_id=pet_id)
+    db_pet = pet_utils.get_pet(db=db, pet_id=pet_id)
     if db_pet is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return pet_db.update_pet(db=db, pet_id=pet_id, pet=pet)
+    return pet_utils.update_pet(db=db, pet_id=pet_id, pet=pet)
 
 
 @router.delete("/{pet_id}")
 def delete_pet(pet_id: int, db: Session = Depends(get_db)):
-    db_pet = pet_db.get_pet(db=db, pet_id=pet_id)
+    db_pet = pet_utils.get_pet(db=db, pet_id=pet_id)
     if db_pet is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    pet_db.delete_pet(db=db, pet_id=pet_id)
+    pet_utils.delete_pet(db=db, pet_id=pet_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
