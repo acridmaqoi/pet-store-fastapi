82c7902c75d39065baea5b06e8f87fa9f045adb6
Sam Quinn <samquinn20@gmail.com>
2022-01-18 15:57:44 +0000

    update db address to updated port

--- app/internal/database.py
+++ app/internal/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5433/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
